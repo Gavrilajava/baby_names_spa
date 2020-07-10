c78f62cf79338215c0c668188741ae7ffbc44af1
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-07-10 09:06:48 -0500

    select starting values

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Select = ({options, onChange, active, title, label }) => {
 
+
+  debugger
   return(
     <>
       <label htmlFor={title}>{label}</label>
       <select 
         name={title} 
         onChange={(e) => onChange(e.target.value)}
-        defaultValue= {{value: active, label: active}}
+        value= {active}
         >
         {options.map(option => <option key = {option} value={option}>{option}</option>)}
       </select>
